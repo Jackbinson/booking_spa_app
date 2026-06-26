666e20d5abadd37d5ff7afe2df7f799a7aadfd04
jackbinson <xuanhungduong18@gmail.com>
2026-06-26 21:11:47 +0700

    feat: add spa and service models

---
+++ lib/models/spa_info.dart
@@ -0,0 +1,17 @@
+class SpaInfo {
+  const SpaInfo({
+    required this.name,
+    required this.address,
+    required this.phone,
+    required this.email,
+    required this.openingHours,
+    required this.description,
+  });
+
+  final String name;
+  final String address;
+  final String phone;
+  final String email;
+  final String openingHours;
+  final String description;
+}
